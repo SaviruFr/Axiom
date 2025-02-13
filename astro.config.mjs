c1625c12dfae61e21239fa02ec577aeb54f6b993
Saviru <147316385+SaviruFr@users.noreply.github.com>
2025-02-13 08:18:42 +0000

    style: refactor components to improve code clarity and consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [
+        tailwind(),
+        sitemap(),
+        robotsTxt(),
+    ],
     output: 'server',
+    site: 'https://axiom-new.pages.dev',
     adapter: cloudflare(),
     build: {
         assets: 'assets'
