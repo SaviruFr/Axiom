a06658c6462de0178ff216bfcb415000ea6b2574
SaviruFr <SaviruFr@users.noreply.github.com>
2025-03-03 18:48:28 +0000

    style: format codebase with prettier

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,18 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
-import { sql } from "drizzle-orm";
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
 
 export const phishingDomains = pgTable('phishing_domain', {
   tenant_id: uuid('tenant_id').notNull(),
-  id: uuid('id').notNull().default(sql`gen_random_uuid()`),
+  id: uuid('id')
+    .notNull()
+    .default(sql`gen_random_uuid()`),
   domain: text('domain').notNull(),
-  date_added: timestamp('date_added').notNull().default(sql`CURRENT_TIMESTAMP`),
-  last_seen: timestamp('last_seen').notNull().default(sql`CURRENT_TIMESTAMP`)
+  date_added: timestamp('date_added')
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  last_seen: timestamp('last_seen')
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
 });
 
 export type PhishingDomain = typeof phishingDomains.$inferSelect;
